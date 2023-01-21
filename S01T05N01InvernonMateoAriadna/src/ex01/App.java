e8aaccc5fdd649706d599c76acc8b844fa60f219
arinvern <69636668+arinvern@users.noreply.github.com>
2023-01-21 16:37:17 +0100

    Add files via upload

---
+++ S01T05N01InvernonMateoAriadna/src/ex01/App.java
@@ -0,0 +1,15 @@
+package ex01;
+
+import org.apache.commons.io.IOUtils;//error
+
+public class App {
+
+	public static void main(String[] args) {
+		
+				
+		String directori=  "C:/Users/Ariadna/Desktop/carpetaS1T5/";
+		
+		Organitza.organitzar(directori);
+	}
+
+}
---
+++ S01T05N01InvernonMateoAriadna/src/ex02/App.java
@@ -0,0 +1,13 @@
+package ex02;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		String directori = "C:/Users/Ariadna/Desktop/carpetaS1T5/";
+
+		Organitza.organitzar(directori);
+
+	}
+
+}
